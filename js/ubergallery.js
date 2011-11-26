76ded5b1fc913171a41f42e943a6c9bf063f58a3
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2011-11-26 14:05:19 -0700

    Top of page button now hidden by default and will show up when scrolled down past the nav bar

--- js/ubergallery.js
+++ js/ubergallery.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
-    
+
     checkTopButtonVisibility();
+    
+    $(window).scroll(function() {
+        checkTopButtonVisibility();
+    });
     
 });
 
-$(window).scroll().checkTopButtonVisibility();
-
-
+/**
+ * Determines wether or not the Top of Page button should be shown or not
+ * based on how far down the page is scrolled.
+ * 
+ * @author Chris Kankiewicz (http://www.ChrisKankiewicz.com)
+ */
 function checkTopButtonVisibility() {
-    if(scrollTop() >= 30) {
-        $('#topButton').hide();
-    } else {
+    if($(window).scrollTop() >= $('#pageNavigation').offset().top) {
         $('#topButton').show();
+    } else {
+        $('#topButton').hide();
     }
 }
