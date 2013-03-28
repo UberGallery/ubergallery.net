6aabeadfaaf6250c418d099cba0885854bcce31c
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-03-28 09:15:40 -0700

    Updated jQuery call to v1.9.1 Updated example colrobox CSS path to no longer have an optional bracket Updated example jQuery call to v1.9.1

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" type="text/css" href="css/UberGallery.css" />
     <link rel="stylesheet" type="text/css" href="demo/resources/colorbox/5/colorbox.css" />
     
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type="text/javascript" src="demo/resources/colorbox/jquery.colorbox.js"></script>
     <script type="text/javascript" src="js/ubergallery.js"></script>
     
@@ -263,10 +263,10 @@
                 <pre>&lt;?php include_once('path/to/resources/UberGallery.php'); $gallery = UberGallery::init()-&gt;createGallery('path/to/images-folder'); ?&gt;</pre>
                 
                 <p>Include the UberGallery and desired Colorbox style sheet in your page header:</p>
-                <pre>&lt;link rel="stylesheet" type="text/css" href="path/to/resources/UberGallery.css" /&gt;<br/>&lt;link rel="stylesheet" type="text/css" href="path/to/resources/colorbox/{1-5}/colorbox.css" /&gt;</pre>
+                <pre>&lt;link rel="stylesheet" type="text/css" href="path/to/resources/UberGallery.css" /&gt;<br/>&lt;link rel="stylesheet" type="text/css" href="path/to/resources/colorbox/1/colorbox.css" /&gt;</pre>
                 
                 <p>Include the jQuery and Colorbox javascript files in your page header:</p>
-                <pre>&lt;script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"&gt;&lt;/script&gt;<br/>&lt;script type="text/javascript" src="path/to/resources/colorbox/jquery.colorbox.js"&gt;&lt;/script&gt;</pre>
+                <pre>&lt;script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"&gt;&lt;/script&gt;<br/>&lt;script type="text/javascript" src="path/to/resources/colorbox/jquery.colorbox.js"&gt;&lt;/script&gt;</pre>
                 
                 <p>Include the Colorbox jQuery call in your header (below the jQuery and Colorbox javascript includes):</p>
                 <pre>&lt;script type="text/javascript"&gt;<br/>$(document).ready(function(){<br/>    $("a[rel='colorbox']").colorbox({maxWidth: "90%", maxHeight: "90%", opacity: ".5"});<br/>});<br/>&lt;/script&gt;</pre>
