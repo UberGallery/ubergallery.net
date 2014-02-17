5e5f14c5523804e49de2120201040405d594ca30
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-02-17 13:34:34 -0700

    Updated Google analytics tracking code.

--- index.php
+++ index.php
@@ -31,19 +31,16 @@
     });
     </script>
 
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-19466324-2']);
-      _gaq.push(['_setDomainName', '.ubergallery.net']);
-      _gaq.push(['_setAllowHash', false]);
-      _gaq.push(['_trackPageview']);
-      _gaq.push(['_trackPageLoadTime']);
+    <script>
 
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-19466324-2', '.ubergallery.net');
+        ga('send', 'pageview');
+
     </script>
 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
